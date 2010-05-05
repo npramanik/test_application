a6026d1cb15a477aa937d78855988959e7a1b9cc
Tripurari Singh <Tripurari@tripurari-singhs-imac.local>
2010-05-05 15:17:28 +0530

    New methode added to the userscontroller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,10 @@
 class UsersController < ApplicationController
+  
+  def new
+    # Used to create new user
+    respond_to do |format|
+      format.html { }    
+      format.js { }
+    end
+  end
 end
