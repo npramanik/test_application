5d5eff25d603b773ebb5f4b9a21662f6893b4179
Nandan <npramanik@crri.co.in>
2010-05-05 15:16:58 +0530

    create a new method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,12 @@
 class UsersController < ApplicationController
-  
   def new
     # Used to create new user
     respond_to do |format|
       format.html { }    
       format.js { }
     end
+  end
+    def show
+    # create a new method
   end
 end
